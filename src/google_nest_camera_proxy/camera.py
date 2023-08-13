6984d7a71b0b28737c7f46a817fd69c1134e9bbc
xev <git@schore.org>
2023-08-13 14:34:50 -0400

    fix: minor fixes

--- src/google_nest_camera_proxy/camera.py
+++ src/google_nest_camera_proxy/camera.py
@@ -14,17 +14,24 @@ from dateutil import tz
 
 
 def _get_legal_camera_name(name: str) -> str:
-    legal_characters = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_.~-"
+    legal_characters = (
+        "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_.~-"
+    )
 
     get_vals = list(filter(lambda x: x in legal_characters, name))
     return "".join(get_vals)
 
 
 class Camera:
-    """ The Nest Camera Class"""
+    """The Nest Camera Class"""
 
-    def __init__(self, configuration: ConfigParser, device: nest.nest.Device,
-                 device_name: str, device_id: str) -> None:
+    def __init__(
+        self,
+        configuration: ConfigParser,
+        device: nest.nest.Device,
+        device_name: str,
+        device_id: str,
+    ) -> None:
 
         # Initial Parameter Settings
 
@@ -44,7 +51,7 @@ class Camera:
 
         # Start the logger
 
-        self._logger = logging.getLogger(f'{self._legal_camera_name}')
+        self._logger = logging.getLogger(f"{self._legal_camera_name}")
 
         # How often we refresh the token. The stream expires after 5 minutes, so no more than 4:30
         self._extension_interval = 4.5 * 60  # 4 minutes 30 seconds
@@ -55,7 +62,7 @@ class Camera:
 
         # Set up the extension timer
         self._timer = threading.Timer(self._extension_interval, self._renew_token)
-        self._timer.setName(f'{self.name} Extension Timer')
+        self._timer.setName(f"{self.name} Extension Timer")
         self._timer.start()
 
         return
@@ -71,12 +78,12 @@ class Camera:
 
     @property
     def name(self) -> str:
-        """Return the camera name. """
-        return self._name.replace(' ', '_')
+        """Return the camera name."""
+        return self._name.replace(" ", "_")
 
     @property
     def stream_url(self) -> str:
-        """ Return the URL """
+        """Return the URL"""
         return self._stream_url
 
     @property
@@ -93,18 +100,20 @@ class Camera:
         self._stream_reset = stream_reset
 
     def _get_token(self) -> None:
-        """ Get the Rtsp Stream Token """
+        """Get the Rtsp Stream Token"""
 
         # Loop until we actually get a token
         while True:
             try:
-                parameters = json.loads('{}')
-                response = self._camera.send_cmd('CameraLiveStream.GenerateRtspStream', parameters)
+                parameters = json.loads("{}")
+                response = self._camera.send_cmd(
+                    "CameraLiveStream.GenerateRtspStream", parameters
+                )
 
-                self._stream_url = response['results']['streamUrls']['rtspUrl']
-                self._streamToken = response['results']['streamToken']
-                self._streamExtensionToken = response['results']['streamExtensionToken']
-                self._expiresAt = response['results']['expiresAt']
+                self._stream_url = response["results"]["streamUrls"]["rtspUrl"]
+                self._streamToken = response["results"]["streamToken"]
+                self._streamExtensionToken = response["results"]["streamExtensionToken"]
+                self._expiresAt = response["results"]["expiresAt"]
 
                 # print(f"Got token:\n"
                 #       f"         StreamURL: {self._stream_url}\n"
@@ -112,20 +121,31 @@ class Camera:
                 #       f"   Extension Token: {self._streamExtensionToken}\n"
                 #       f"        Expires At: {self._expiresAt}")
 
-                self._expiration_time = parser.isoparse(response['results']['expiresAt']).astimezone(tz.tzlocal())
-                message = 'Got token for {}, expiration time: {} - time to expiration = {}'.format(
-                    self.name, str(self._expiration_time).split('.')[0],
-                    str(self._expiration_time - datetime.datetime.now(tz.tzlocal())).split('.')[0])
+                self._expiration_time = parser.isoparse(
+                    response["results"]["expiresAt"]
+                ).astimezone(tz.tzlocal())
+                message = "Got token for {}, expiration time: {} - time to expiration = {}".format(
+                    self.name,
+                    str(self._expiration_time).split(".")[0],
+                    str(
+                        self._expiration_time - datetime.datetime.now(tz.tzlocal())
+                    ).split(".")[0],
+                )
                 self._logger.info(message)
 
                 # We succeeded in getting the token, so break the loop
                 break
 
             except nest.APIError as error:
-                self._logger.error(f"Received a Nest APIError, authenticating camera {error=}. Sleeping for a bit ...")
+                self._logger.error(
+                    f"Received a Nest APIError, authenticating camera {error=}. "
+                    f"Sleeping and trying again."
+                )
 
             except Exception as error:
-                self._logger.error(f"Unexpected error getting token {error=}, {type(error)=}")
+                self._logger.error(
+                    f"Unexpected error getting token {error=}, {type(error)=}"
+                )
 
             # Wait till rate limit goes away
             time.sleep(30)
@@ -133,7 +153,7 @@ class Camera:
         return
 
     def _renew_token(self) -> None:
-        """ Renew the camera's token """
+        """Renew the camera's token"""
 
         # Check to see if we were requested to terminate the process
 
@@ -142,11 +162,17 @@ class Camera:
             return
 
         try:
-            json_string = '{"streamExtensionToken" : "' + self._streamExtensionToken + '"}'
+            json_string = (
+                '{"streamExtensionToken" : "' + self._streamExtensionToken + '"}'
+            )
             parameter = json.loads(json_string)
-            extend_response = self._camera.send_cmd('CameraLiveStream.ExtendRtspStream', parameter)
-            self._streamExtensionToken = extend_response['results']['streamExtensionToken']
-            self._expiresAt = extend_response['results']['expiresAt']
+            extend_response = self._camera.send_cmd(
+                "CameraLiveStream.ExtendRtspStream", parameter
+            )
+            self._streamExtensionToken = extend_response["results"][
+                "streamExtensionToken"
+            ]
+            self._expiresAt = extend_response["results"]["expiresAt"]
 
             # print(f"Got token:\n"
             #       f"         StreamURL: {self._stream_url}\n"
@@ -154,18 +180,26 @@ class Camera:
             #       f"   Extension Token: {self._streamExtensionToken}\n"
             #       f"        Expires At: {self._expiresAt}")
 
-            self._expiration_time = parser.isoparse(extend_response['results']['expiresAt']).astimezone(tz.tzlocal())
-            message = f"Renewed token for {self.name} expiration time: {str(self._expiration_time).split('.')[0]}" \
-                      f" - time to expiration = " \
-                      f"{str(self._expiration_time - datetime.datetime.now(tz.tzlocal())).split('.')[0]}"
+            self._expiration_time = parser.isoparse(
+                extend_response["results"]["expiresAt"]
+            ).astimezone(tz.tzlocal())
+            message = (
+                f"Renewed token for {self.name} expiration time: {str(self._expiration_time).split('.')[0]}"
+                f" - time to expiration = "
+                f"{str(self._expiration_time - datetime.datetime.now(tz.tzlocal())).split('.')[0]}"
+            )
             self._logger.info(message)
 
         except nest.APIError as error:
-            self._logger.error(f"Received a Nest APIError, re-authenticating camera {error=}")
+            self._logger.error(
+                f"Received a Nest APIError, re-authenticating camera {error=}. Getting new token."
+            )
             self._get_token()
 
         except Exception as error:
-            self._logger.error(f"Unexpected error extending token {error=}, {type(error)=}")
+            self._logger.error(
+                f"Unexpected error extending token {error=}, {type(error)=}"
+            )
             self._get_token()
 
         # Add the next extension timer
@@ -173,7 +207,7 @@ class Camera:
         self._timer.start()
 
     def terminate(self) -> None:
-        """ Indicate that the threads should terminate """
+        """Indicate that the threads should terminate"""
         self._logger.warning(f"<{self.name}> Terminating")
         self._terminate_signal.set()
         self._timer.cancel()
--- src/google_nest_camera_proxy/rtsp_server.py
+++ src/google_nest_camera_proxy/rtsp_server.py
@@ -11,7 +11,7 @@ from .camera import Camera
 
 
 class RTSPServer:
-    """ Manage the RTSP server. This can run in one of two ways, a single rtsp server, or one for each camera """
+    """Manage the RTSP server. This can run in one of two ways, a single rtsp server, or one for each camera"""
 
     def __init__(self, configuration: configparser.ConfigParser) -> None:
 
@@ -28,12 +28,15 @@ class RTSPServer:
         # timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
         # shutil.copyfile(self._filepath, f"{self._dir}/backup/{self._filename}.{timestamp}")
 
-        self._config_filename = self._configuration.get('RTSP_SERVER', 'config_filename',
-                                                        fallback=f"{os.path.expanduser('~')}/.config/nest/rtsp.yml")
-        self._executable = configuration.get('RTSP_SERVER', 'executable')
+        self._config_filename = self._configuration.get(
+            "RTSP_SERVER",
+            "config_filename",
+            fallback=f"{os.path.expanduser('~')}/.config/nest/rtsp.yml",
+        )
+        self._executable = configuration.get("RTSP_SERVER", "executable")
 
         # Set up logging
-        self._logger = logging.getLogger(f'RTSP Server')
+        self._logger = logging.getLogger(f"RTSP Server")
 
         # Set up a signal if we want to terminate the process
         self._terminate_signal = threading.Event()
@@ -42,7 +45,7 @@ class RTSPServer:
 
         # Start the thread that updates the configuration file
         self._update_thread = threading.Thread(target=self._update_configuration)
-        self._update_thread.setName('RTSP Configuration Update')
+        self._update_thread.setName("RTSP Configuration Update")
         self._update_thread.start()
 
         return
@@ -50,17 +53,17 @@ class RTSPServer:
     def run(self):
         # Run the server in a different thread
         self._rtsp_thread = threading.Thread(target=self._run_server)
-        self._rtsp_thread.setName('RTSP Server')
+        self._rtsp_thread.setName("RTSP Server")
         self._rtsp_thread.start()
 
     def add_camera(self, camera: Camera) -> None:
-        """ Add a camera to the list of cameras"""
+        """Add a camera to the list of cameras"""
 
         self._camera_list[camera.legal_camera_name] = camera
         return
 
     def _update_configuration(self):
-        """ Check to see if any cameras have updated configurations, and if so, update the configuration file """
+        """Check to see if any cameras have updated configurations, and if so, update the configuration file"""
 
         # Wait a couple of minutes to allow the cameras to get added
         time.sleep(120)
@@ -79,25 +82,29 @@ class RTSPServer:
             time.sleep(60)
 
     def _run_server(self):
-        """ Run a single RTSP server """
+        """Run a single RTSP server"""
 
+        self._logger.warning("Starting RTSP Server")
         # Set up regular expressions
         re_not_publishing = re.compile(r"no one is publishing to path '([^']*)")
-        re_source_ready = re.compile(r"\[path ([^\]]*)\] \[rtsp source\] ready")
+        re_source_ready = re.compile(r"\[path ([^\]]*)\] \[RTSP source\] ready")
         re_reading_from_path = re.compile(r"is reading from path '([^']*)'")
 
         # Set up the metrics for the cameras
         number_not_published = {}
         for camera in self._camera_list.values():
             number_not_published[camera.legal_camera_name] = 0
-            self._status[camera.legal_camera_name] = 'Initializing'
+            self._status[camera.legal_camera_name] = "Initializing"
 
         # Write the configuration file
         self.write_configuration_file()
 
         # Start the RTSP process
-        self._subprocess = subprocess.Popen([self._executable, self._config_filename],
-                                            stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        self._subprocess = subprocess.Popen(
+            [self._executable, self._config_filename],
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+        )
 
         while True:
             # Check to see if we are being asked to terminate
@@ -105,7 +112,7 @@ class RTSPServer:
                 self._subprocess.terminate()
                 return
 
-            line = self._subprocess.stdout.readline().decode('utf-8').strip()
+            line = self._subprocess.stdout.readline().decode("utf-8").strip()
             if not line:
                 break
             self._logger.info(f"RTSP: {line}")
@@ -113,32 +120,37 @@ class RTSPServer:
             if re_match:
                 camera_name = re_match.group(1)
                 if camera_name not in self._status.keys():
-                    self._status[camera_name] = 'Initializing'
+                    self._status[camera_name] = "Initializing"
                     number_not_published[camera_name] = 0
 
                 self._status[camera_name] = "Not Publishing"
                 number_not_published[camera_name] += 1
-                self._logger.info(
-                    f"<{camera_name}> No one publishing, incremented to {number_not_published[camera_name]}")
+                self._logger.warning(
+                    f"<{camera_name}> No one publishing, incremented to {number_not_published[camera_name]}"
+                )
 
             re_match = re_source_ready.search(line)
             if re_match:
                 camera_name = re_match.group(1)
                 if camera_name not in self._status.keys():
-                    self._status[camera_name] = 'Initializing'
+                    self._status[camera_name] = "Initializing"
                     number_not_published[camera_name] = 0
 
                 self._status[camera_name] = "Publishing"
                 number_not_published[camera_name] = 0
-                self._logger.warning(f"<{camera_name}> started publishing after "
-                                     f"{number_not_published[camera_name]} attempts")
+                self._logger.warning(
+                    f"<{camera_name}> started publishing after "
+                    f"{number_not_published[camera_name]} attempts"
+                )
 
             re_match = re_reading_from_path.search(line)
             if re_match:
                 camera_name = re_match.group(1)
-                self._logger.warning(f"<{camera_name}> Someone started reading the stream")
+                self._logger.warning(
+                    f"<{camera_name}> Someone started reading the stream"
+                )
                 if camera_name not in self._status.keys():
-                    self._status[camera_name] = 'Initializing'
+                    self._status[camera_name] = "Initializing"
                     number_not_published[camera_name] = 0
 
                 self._status[camera_name] = "Streaming"
@@ -147,7 +159,9 @@ class RTSPServer:
             for camera_name in number_not_published.keys():
                 if number_not_published[camera_name] > 5:
                     self._status[camera_name] = "Resetting"
-                    self._logger.warning(f"<{camera_name}> Too many failures, trying to reset")
+                    self._logger.warning(
+                        f"<{camera_name}> Too many failures, trying to reset"
+                    )
                     self._camera_list[camera_name].reset()
                     number_not_published[camera_name] = 0
 
@@ -161,11 +175,11 @@ class RTSPServer:
             return
 
     def _get_base_file(self) -> str:
-        """ Read the file and save the lines prior to our edits"""
+        """Read the file and save the lines prior to our edits"""
 
-        prefix: str = ''
+        prefix: str = ""
         found_separator = False
-        with open(self._config_filename, 'r') as file:
+        with open(self._config_filename, "r") as file:
             for line in file.readlines():
 
                 # We need to add the username and password to the configuration file for each camera, but rather than
@@ -190,7 +204,7 @@ class RTSPServer:
         return prefix
 
     def write_configuration_file(self):
-        """ Write the file with all the camera information """
+        """Write the file with all the camera information"""
 
         self._logger.info("Writing configuration file")
         self._write_lock.acquire()
@@ -199,11 +213,13 @@ class RTSPServer:
             file_contents = str()
             for camera_name in sorted(self._camera_list.keys()):
                 camera = self._camera_list[camera_name]
-                file_contents = (f"{file_contents}\n"
-                                 f"  {camera.legal_camera_name}:\n"
-                                 f"    source: {camera.stream_url}\n"
-                                 f"    readUser: {self._read_user}\n"
-                                 f"    readPass: {self._read_pass}\n\n")
+                file_contents = (
+                    f"{file_contents}\n"
+                    f"  {camera.legal_camera_name}:\n"
+                    f"    source: {camera.stream_url}\n"
+                    f"    readUser: {self._read_user}\n"
+                    f"    readPass: {self._read_pass}\n\n"
+                )
             with open(self._config_filename, "w") as file:
                 file.write(prefix + file_contents)
         except Exception as error:
@@ -212,7 +228,7 @@ class RTSPServer:
             self._write_lock.release()
 
     def terminate(self) -> None:
-        """ Indicate that the threads should terminate """
+        """Indicate that the threads should terminate"""
         self._logger.warning(f"RTSP Terminating")
         self._terminate_signal.set()
 
